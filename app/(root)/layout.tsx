e8060f7889d513614dc3404ecb3f717e7fab7ec6
Suryansh Choudhary <suryansh.choudhary999@gmail.com>
2024-07-09 02:39:28 +0530

    Implement website layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Sidebar from "@/components/Sidebar";
+import Image from 'next/image'
+import MobileNav from "@/components/MobileNav";
 
 
 export default function RootLayout({
@@ -5,10 +8,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn={firstName: 'Suryansh', lastName: 'JSM'};
+
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src="/icons/logo.svg"
+              width={30}
+              height={30}
+              alt="logo"
+
+            />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+
+          </div>
+          {children}
+
+        </div>
     </main>
   );
 }
